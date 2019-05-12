d89892bec95aff38e2c86d78fcfc1f790b104a3b
Harkishen-Singh <harkishensingh@hotmail.com>
2019-05-12 12:30:41 +0530

    added missing languages

--- speech.js
+++ speech.js
@@ -10,7 +10,12 @@ speech.onresult = function (eve) {
     }
 };
 
-var languages = {
+const element = document.getElementById('language-selection-area');
+let selectElement = document.createElement('select');
+selectElement.style.maxWidth = '100%';
+element.appendChild(selectElement);
+
+const languages = {
     'af-ZA': 'Afrikaans',
     'id-ID': 'Bahasa Indonesia',
     'ms-MY': 'Bahasa Melayu',
@@ -44,6 +49,32 @@ var languages = {
     'es-DO': 'República Dominicana',
     'es-UY': 'Uruguay',
     'es-VE': 'Venezuela',
+    'mr-IN': 'मराठी',
+    'hu-HU': 'Magyar',
+    'lo-LA': 'ລາວ',
+    'nl-NL': 'Nederlands',
+    'ne-NP': 'नेपाली भाषा',
+    'nb-NO': 'Norsk bokmål',
+    'si-LK': 'සිංහල',
+    'sl-SI': 'Slovenščina',
+    'su-ID': 'Basa Sunda',
+    'sw-TZ': 'Kiswahili - Tanzania',
+    'sw-KE': 'Kiswahili - Kenya',
+    'ka-GE': 'ქართული',
+    'hy-AM': 'Հայերեն',
+    'ta-IN': 'தமிழ் - இந்தியா',
+    'ta-SG': 'தமிழ் - சிங்கப்பூர்',
+    'ta-LK': 'தமிழ் - இலங்கை',
+    'ta-MY': 'தமிழ் - மலேசியா',
+    'te-IN': 'తెలుగు',
+    'vi-VN': 'Tiếng Việt',
+    'tr-TR': 'Türkçe',
+    'ur-PK': 'اُردُو - پاکستان',
+    'ur-IN': 'اُردُو - بھارت',
+    'el-GR': 'Ελληνικά',
+    'uk-UA': 'Українська',
+    'hi-IN': 'हिन्दी',
+    'th-TH': 'ภาษาไทย',
     'eu-ES': 'Euskara',
     'fr-FR': 'Français',
     'gl-ES': 'Galego',
@@ -52,9 +83,6 @@ var languages = {
     'is-IS': 'Íslenska',
     'it-IT': 'Italiano-Italia',
     'it-CH': 'Italiano-Svizzera',
-    'hu-HU': 'Magyar',
-    'nl-NL': 'Nederlands',
-    'nb-NO': 'Norsk bokmål',
     'pl-PL': 'Polski',
     'pt-BR': 'Português-Brasil',
     'pt-PT': 'Português-Portugal',
@@ -62,7 +90,6 @@ var languages = {
     'sk-SK': 'Slovenčina',
     'fi-FI': 'Suomi',
     'sv-SE': 'Svenska',
-    'tr-TR': 'Türkçe',
     'bg-BG': 'български',
     'ru-RU': 'Pусский',
     'sr-RS': 'Српски',
@@ -72,8 +99,25 @@ var languages = {
     'cmn-Hant-TW': '中文 - 中文 (台灣)',
     'yue-Hant-HK': '中文 - 粵語 (香港)',
     'ja-JP': '日本語',
-    'la': 'Lingua latīna'
+    'la': 'Lingua latīna',
+    'jv-ID': 'Basa Jawa',
+    'fil-PH': 'Filipino',
+    'gu-IN': 'ગુજરાતી',
+    'kn-IN': 'ಕನ್ನಡ',
+    'km-KH': 'ភាសាខ្មែរ',
+    'lv-LV': 'Latviešu',
+    'lt-LT': 'Lietuvių',
+    'ml-IN': 'മലയാളം'
 };
+
+for( var lang in languages) {
+    let optionELement = document.createElement('option');
+    console.log('lang is ', lang);
+    optionELement.value = lang;
+    optionELement.text = languages[lang];
+    selectElement.appendChild(optionELement);
+    optionELement = null;
+}
 
 function initiateRecording(val) {
     iteration += val;
